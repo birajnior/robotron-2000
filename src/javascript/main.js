c79662d15b0d999a97fb9cb7f2ca0d4e75a3974b
birajnior <ubiratanbernardo@gmail.com>
2023-06-22 23:15:43 -0300

    Adicionando peças do robô

---
+++ src/javascript/main.js
@@ -0,0 +1,16 @@
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach((element) => {
+  element.addEventListener("click", () => {
+    manipulaDados(event.target.textContent, event.target.parentNode);
+  });
+});
+
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector(".controle-contador");
+  if (operacao === "-") {
+    peca.value = parseInt(peca.value) - 1;
+  } else {
+    peca.value = parseInt(peca.value) + 1;
+  }
+}
